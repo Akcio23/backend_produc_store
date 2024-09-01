dc8b161bd993b7abcefdd7911ef37d6444793bc5
Akcio23 <dev.akcio@gmail.com>
2024-08-31 23:49:31 -0300

    Adding routes and adjusting data model

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const port = 3000;
 // Configurando modelo de dados
 const Product = mongoose.model('Product', { 
     name: String,
-    description: String,
+    brand: String,
+    category: String,
     price: Number,
     quantity: Number,
     image_url: String
@@ -30,15 +31,35 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     const product = new Product({
         name: req.body.name,
-        description: req.body.description,
+        brand: req.body.brand,
+        category: req.body.category,
         price: req.body.price,
         quantity: req.body.quantity,
         image_url: req.body.image_url
     })
     await product.save()
-    res.status(201).send('Product added successfully')
+    res.status(201).send(product)
 });
 
+// Rota DELETE Product
+app.delete('/:id', async(req ,res)=>{
+   await Product.findByIdAndDelete(req.params.id)
+  res.status(200).send("Deletado com sucesso!")
+})
+
+// Rota PUT Product
+
+app.put('/:id', async (req, res) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      brand: req.body.brand,
+      category: req.body.category,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      image_url: req.body.image_url
+  },{new:true})
+    res.status(200).send(product)
+})
 
 app.listen(port, () => {
 
